7c932ae2029ee0b5208fa73700c022c8bb8f8195
Money00111 <niyigenaepizo9@gmail.com>
2026-06-06 18:03:59 +0200

    Remove Firebase config and initialization code
    Removed Firebase configuration and database initialization.

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
 import {
 getAuth,
 createUserWithEmailAndPassword,
