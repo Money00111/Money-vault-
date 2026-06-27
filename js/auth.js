daf24531a768b1956ea5fd270b910113a6985435
Money00111 <niyigenaepizo9@gmail.com>
2026-06-27 23:36:31 +0200

    Add loginUser function for user authentication

--- js/auth.js
+++ js/auth.js
@@ -34,3 +34,17 @@ export function registerUser(email, password, name, phone, referral) {
       alert(error.message);
     });
   }
+
+export function loginUser(email, password) {
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+
+      alert("Login successful!");
+      window.location.href = "dashboard.html";
+
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
