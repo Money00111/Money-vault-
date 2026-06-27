582399af39b01d82752ed89691c7bb5ed2bfe362
Money00111 <niyigenaepizo9@gmail.com>
2026-06-27 23:32:25 +0200

    Refactor Firebase config and imports
    Updated Firebase configuration keys and restructured imports.

--- js/firebase.js
+++ js/firebase.js
@@ -1,34 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
 
+// 🔥 Firebase config (UZASHYIRAMO YOUR DATA)
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  databaseURL: "YOUR_DB",
-  projectId: "YOUR_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
 
+// Init Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
 export const auth = getAuth(app);
-
-import { auth, db } from "./firebase.js";
-import { onAuthStateChanged } from "firebase/auth";
-import { ref, onValue, push } from "firebase/database";
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) return (window.location.href = "login.html");
-
-  const uid = user.uid;
-
-  // BALANCE
-  onValue(ref(db, "users/" + uid + "/balance"), (snap) => {
-    document.getElementById("balanceBox").innerText =
-      "Balance: " + (snap.val() || 0) + " RWF";
-  });
-});
+export const db = getDatabase(app);
