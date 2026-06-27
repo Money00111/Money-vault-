bb3a86181e6f14d220985868717247365bdad4cc
Money00111 <niyigenaepizo9@gmail.com>
2026-06-27 23:57:19 +0200

    Implement authentication guard and user data display

---
+++ js/dashboard.js
@@ -0,0 +1,38 @@
+import { auth, db } from "./firebase.js";
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+import {
+  ref,
+  onValue
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+
+
+// 🔐 AUTH GUARD
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const uid = user.uid;
+
+  // USER DATA
+  const userRef = ref(db, "users/" + uid);
+
+  onValue(userRef, (snapshot) => {
+
+    const data = snapshot.val();
+
+    if (data) {
+      document.getElementById("username").innerText = data.name;
+      document.getElementById("balance").innerText = data.balance + " RWF";
+      document.getElementById("refCode").innerText = data.referralCode;
+    }
+
+  });
+
+});
