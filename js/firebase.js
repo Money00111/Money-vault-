d96c9333f56afcc7616f4fa97272aa1b1a6c3103
Money00111 <niyigenaepizo9@gmail.com>
2026-06-28 10:13:15 +0200

    Update Firebase config with real values
    Updated Firebase configuration with actual credentials.

--- js/firebase.js
+++ js/firebase.js
@@ -4,13 +4,14 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase
 
 // 🔥 Firebase config (UZASHYIRAMO YOUR DATA)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC0ugw0iH2h00bJxxHq7qMRBvYYmFjPqCU",
+  authDomain: "money-vault-c48d3.firebaseapp.com",
+  databaseURL: "https://money-vault-c48d3-default-rtdb.firebaseio.com",
+  projectId: "money-vault-c48d3",
+  storageBucket: "money-vault-c48d3.firebasestorage.app",
+  messagingSenderId: "1068478656241",
+  appId: "1:1068478656241:web:06b698f04f3382a9784350",
+  measurementId: "G-FBBMY3L8YF"
 };
 
 // Init Firebase
