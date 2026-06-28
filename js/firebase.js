f6a72e53e7b69b42977be7fb148c391bf9844a29
Money00111 <niyigenaepizo9@gmail.com>
2026-06-28 10:18:11 +0200

    Refactor Firebase config and initialization

--- js/firebase.js
+++ js/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
 
-// 🔥 Firebase config (UZASHYIRAMO YOUR DATA)
 const firebaseConfig = {
   apiKey: "AIzaSyC0ugw0iH2h00bJxxHq7qMRBvYYmFjPqCU",
   authDomain: "money-vault-c48d3.firebaseapp.com",
@@ -10,11 +9,9 @@ const firebaseConfig = {
   projectId: "money-vault-c48d3",
   storageBucket: "money-vault-c48d3.firebasestorage.app",
   messagingSenderId: "1068478656241",
-  appId: "1:1068478656241:web:06b698f04f3382a9784350",
-  measurementId: "G-FBBMY3L8YF"
+  appId: "1:1068478656241:web:06b698f04f3382a9784350"
 };
 
-// Init Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
