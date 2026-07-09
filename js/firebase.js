dc605a6664f32cc52b36fca36791121c1e922cbd
Money00111 <niyigenaepizo9@gmail.com>
2026-07-09 07:38:46 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,18 +2,13 @@
 // FIREBASE CONFIG
 // ======================================
 
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
-import { getAuth } 
-from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
-import { getDatabase } 
-from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
-import { getStorage } 
-from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
-
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
 // ======================================
 // CONFIG
@@ -25,20 +20,15 @@ const firebaseConfig = {
 
   authDomain: "money-vault-c48d3.firebaseapp.com",
 
-  databaseURL: 
-  "https://money-vault-c48d3-default-rtdb.firebaseio.com",
-
   projectId: "money-vault-c48d3",
 
-  storageBucket:
-  "money-vault-c48d3.appspot.com",
+  storageBucket: "money-vault-c48d3.appspot.com",
 
   messagingSenderId: "1068478656241",
 
-  appId:
-  "1:1068478656241:web:aacbcf12922a21fe784350"
-};
+  appId: "1:1068478656241:web:aacbcf12922a21fe784350"
 
+};
 
 // ======================================
 // INIT FIREBASE
@@ -48,10 +38,9 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-const db = getDatabase(app);
+const db = getFirestore(app);
 
 const storage = getStorage(app);
-
 
 // ======================================
 // EXPORT
